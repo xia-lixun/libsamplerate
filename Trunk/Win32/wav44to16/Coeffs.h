b8b3d32c8ca63f5d587f21d7e08dc1c2a57a678b
lixun <lixun.xia@yahoo.com>
2016-12-07 23:14:29 +0800

    48khz to 16khz resample is realized via changing constants in Coeff.h

--- Trunk/Win32/wav44to16/Coeffs.h
+++ Trunk/Win32/wav44to16/Coeffs.h
@@ -3,9 +3,11 @@
 
 //resample_fs = fs * P / Q
 #define TARGET_SAMPLE_RATE  (16000)  //Hz
-#define SOURCE_SAMPLE_RATE  (44100)  //Hz
+#define SOURCE_SAMPLE_RATE  (441000)  //Hz
+//#define SOURCE_SAMPLE_RATE  (48000)  //Hz
 
 const double FirCoeffs[] =
 {
 #include "FirCoeff44To16.dat"
+//#include "FirCoeff48To16.dat"
 };
